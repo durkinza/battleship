1ebe2beeee9d423e75e0011da4b1b600cd72c8d5
Zane Durkin <zanedurkin@gmail.com>
2016-03-13 16:11:31 -0600

    created ships as objects
    ships are onjects so they can have their own properties

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Authors: Tanner Purves, Zane Durkin
+from distutils.core import setup
+
+setup(name='battleship',
+      version='1.0',
+      description='Battleship game',
+      author='Zane Durkin',
+      author_email='zanedurkin@gmail.com',
+      url='https://github.com/durkinza/battleship',
+      packages=['prettyTable', 'configobj',],
+     )
